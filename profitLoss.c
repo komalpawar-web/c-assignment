c955e0acf2bbf8c5e6ca41f6e940458ac67dd294
komal pawar <komalpawar5752@gmail.com>
2025-06-20 20:36:15 +0530

    assighnmrnt2

--- profitLoss.c
+++ profitLoss.c
@@ -1,24 +1,26 @@
-#include<stdio.h>
-int main(){
-    float cp,sp,profit,loss;
+#include <stdio.h>
+int main()
+{
+    float cp, sp, profit, loss;
     printf("Enter the cost price:");
-    scanf("%f",&cp);
+    scanf("%f", &cp);
     printf("\nEnter the selling price:");
-    scanf("%f",&sp);
-    
-    
-    if(sp>cp){
-        profit=sp-cp;
-        printf("profit=%.2f",profit);
-    }
-    else if(cp>sp){
-    printf("Loss=%.2f",loss);
-    loss=cp-sp;
-    }
-    else{
-    printf("\nNo profit No loss.");
-    }
+    scanf("%f", &sp);
 
+    if (sp > cp)
+    {
+        profit = sp - cp;
+        printf("profit=%.2f", profit);
+    }
+    else if (cp > sp)
+    {
+        loss = cp - sp;
+        printf("Loss=%.2f", loss);
+    }
+    else
+    {
+        printf("\nNo profit No loss.");
+    }
 
     return 0;
 }
