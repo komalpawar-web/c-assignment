ac9e2b7f1d057ff0161c812a8690d7d95536510f
komal pawar <komalpawar5752@gmail.com>
2025-06-26 22:11:46 +0530

    Assignment4

---
+++ q21.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int i,n,rem;
+    printf("Enter a number:");
+    scanf("%d",&n);
+    while(n>0)
+    {
+        rem=n%10;
+        printf("%d  ",rem);
+        n=n/10;
+    }
+    return 0;
+}
