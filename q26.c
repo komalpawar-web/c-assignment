275cf638c94f5d18a56ca3021796d7b44c2b288e
komal pawar <komalpawar5752@gmail.com>
2025-06-27 19:54:21 +0530

    Asignment4

--- q26.c
+++ q26.c
@@ -1,14 +1,17 @@
-#include<stdio.h>
+#include <stdio.h>
 int main()
 {
-    int n,rev;
+    int n, rev = 0;
+
     printf("Enter a number:");
-    scanf("%d",&n);
-    while(n>0)
+    scanf("%d", &n);
+    int temp = n;
+    while (n > 0)
     {
-        rev=n%10;
-        printf("%d",rev);
-        n=n/10;
+        int rem = n % 10;
+        rev = rev * 10 + rem;
+        n = n / 10;
     }
+    printf("\nReverse of %d is %d", temp, rev);
     return 0;
 }
