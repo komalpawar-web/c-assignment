fd5a9fe4dd6a0f981ca86fcdfa7b9fb595a14415
komal pawar <komalpawar5752@gmail.com>
2025-06-27 23:01:43 +0530

    Assignment4

--- q28.c
+++ q28.c
@@ -6,24 +6,24 @@ int main() {
     printf("Enter a number: ");
     scanf("%d", &num);
 
-    // If number is 0, just print Zero
+    
     if (num == 0) {
         printf("Zero");
         return 0;
     }
 
-    // If number is negative, make it positive
+    
     if (num < 0)
         num = -num;
 
-    // Reverse the number first to print from left to right
+    
     int temp = num;
     while (temp > 0) {
         rev = rev * 10 + temp % 10;
         temp /= 10;
     }
 
-    // Now print digits in words
+   
     while (rev > 0) {
         digit = rev % 10;
 
